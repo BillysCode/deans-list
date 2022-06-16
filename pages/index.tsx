bb4fe20b647be5f275bfd62273e127fef3ea2931
bobthegrownup <47165999+bobthegrownup@users.noreply.github.com>
2022-06-16 19:22:48 +0300

    Update index.tsx
    updated the intro text thank you!

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,9 +48,11 @@ const Home: NextPage = () => {
             <VStack spacing={4} w="full" align="left">
               <Heading>Dean&apos;s List</Heading>
               <Text>
-                Use $DEAN to vote on projects to get feedback. You can purchase
+                Use $DEAN to vote on projects to get feedback from the Dean's List DAO. You can purchase
                 some $DEAN below. At the end of the cycle, the project with the
-                most $DEAN will be reviewed. Reviews will happen in the Grape Discord.
+                most $DEAN will be reviewed if it is at least 50 $DEAN. 
+                All other projects will be reviewed by the DAO's discretion. 
+                Reviews will happen in the Dean's List Discord here https://discord.gg/ucNrp7bSwM .
               </Text>
               <Center>
                 <Box
